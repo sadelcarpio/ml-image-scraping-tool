fc1a0f1b8a79997c002647a30507b60cb18cf070
sadelcarpio <sadelcarpioa@gmail.com>
2023-10-17 13:24:08 -0500

    Continue on local laptop

--- image_scraper/image_scraper/spiders/images_spider.py
+++ image_scraper/image_scraper/spiders/images_spider.py
@@ -1,6 +1,7 @@
 import time
 
 import scrapy
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -11,6 +12,7 @@ from ..items import ImageItem
 
 class GoogleImagesSpider(scrapy.Spider):
     name = "google_images_spider"
+    # large images published on the last 24 hrs
     start_urls = [
         "https://www.google.com/search?q=real+cats+images&tbm=isch&tbs=qdr:d%2Cisz:l",
     ]
@@ -20,21 +22,61 @@ class GoogleImagesSpider(scrapy.Spider):
         # Extract the image URLs from the Google Images page.
         # Scrape the image data.
         driver: WebDriver = response.meta['driver']
-        images_per_load = len(response.xpath('//*[@id="islrg"]/div[1]/div/a[1]/div[1]/img').getall())
-        for i in range(1, images_per_load + 1):
-            driver.find_element(By.XPATH, f'//*[@id="islrg"]/div[1]/div[{i}]/a[1]/div[1]/img').click()
-            # Could work but need to validate if image loaded completely (not pixelated)
-            # img_element = WebDriverWait(driver, 10).until(
-            #  expected_conditions.presence_of_element_located((By.XPATH, f'{self.base_path}/div[3]/div[1]/a/img[1]'))
-            # )
-            time.sleep(3)
-            img_element = driver.find_element(By.XPATH, f'{self.base_path}/div[3]/div[1]/a/img[1]')
+        time.sleep(3)
+        initial_load = len(response.xpath('//*[@id="islrg"]/div[1]/div/a[1]/div[1]/img').getall())
+        i = 1
+        while True:
+            try:
+                thumbnail_img = driver.find_element(By.XPATH, f'//*[@id="islrg"]/div[1]/div[{i}]/a[1]/div[1]/img')
+                driver.execute_script('arguments[0].click()', thumbnail_img)
+            except NoSuchElementException:
+                break
+            time.sleep(0.1)
+            img_element = driver.find_element(By.XPATH, f'{self.base_path}//a/img[1]')
             img_src = img_element.get_attribute('src')
             if not img_src.startswith('data:image'):
                 yield ImageItem(image_urls=[img_src])
+            driver.find_element(By.XPATH, f'{self.base_path}//div[1]/div/div[2]/div[3]/button').click()
+            i += 1
+            if i % initial_load == 0:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(3)
+        self.parse_scrolled(response, i)
 
-        # TODO: Logic for handling infinite scrolling on results
-        # next_page = response.xpath("//tbody/td/a/@href").get() or response.xpath(
-        #     "//tbody/tr/td/a/span[text()='>']/parent::a/@href").get()
-        # if next_page is not None:
-        #     yield response.follow(next_page, self.parse)
+    def parse_scrolled(self, response, i):
+        driver = response.meta['driver']
+        initial_load = len(response.xpath(f'//*[@id="islrg"]/div[1]/div[{i}]/div/a[1]/div[1]/img').getall())
+        j = 1
+        while True:
+            try:
+                driver.find_element(By.XPATH, f'//*[@id="islrg"]/div[1]/div[{i}]/div[{j}]/a[1]/div[1]/img').click()
+            except NoSuchElementException:
+                self.parse_scrolled(response, i + 1)
+            time.sleep(3)
+            img_element = driver.find_element(By.XPATH, f'{self.base_path}//a/img[1]')
+            img_src = img_element.get_attribute('src')
+            if not img_src.startswith('data:image'):
+                yield ImageItem(image_urls=[img_src])
+            driver.find_element(By.XPATH, f'{self.base_path}//div[1]/div/div[2]/div[3]/button').click()
+            if i % (initial_load - 1) == 0:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(3)
+
+# before scrolling
+# //*[@id="islrg"]/div[1]/div[47]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[48]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[37]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[46]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[49]/a[1]/div[1]/img
+# after scrolling (increases an extra div)
+# //*[@id="islrg"]/div[1]/div[51]/div[1]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[51]/div[2]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[51]/div[3]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[51]/div[4]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[51]/div[13]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[51]/div[24]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[51]/div[38]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[51]/div[54]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[52]/div[3]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[52]/div[70]/a[1]/div[1]/img
+# //*[@id="islrg"]/div[1]/div[53]/div[18]/a[1]/div[1]/img
