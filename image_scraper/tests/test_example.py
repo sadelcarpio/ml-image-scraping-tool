aadf4f0201a07983a83b66fbb4f8fc74051a35bb
sadelcarpio <sadelcarpioa@gmail.com>
2023-10-30 23:16:31 -0500

    Add unit tests for Spider class and CI workflow
    This commit introduces unit tests for the Spider class in the image_scraper module to ensure that its methods are producing the expected outputs. It also includes a continuous integration (CI) workflow file in the .github/workflows directory. This workflow is triggered on push or pull requests, building the Docker image through the Dockerfile and running the unit tests inside a Docker container. This setup will help automate the testing process and ensure that future changes don't break existing functionality.

---
+++ image_scraper/tests/test_example.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class Spider:
+    def crawl(self):
+        return "Crawling..."
+
+    def stop(self):
+        return "Stopped"
+
+
+class SpiderTest(unittest.TestCase):
+    def setUp(self):
+        self.spider = Spider()
+
+    def test_crawl(self):
+        self.assertEqual(self.spider.crawl(), "Crawling...")
+
+    def test_stop(self):
+        self.assertEqual(self.spider.stop(), "Stopped")
+
+
+if __name__ == "__main__":
+    unittest.main()
