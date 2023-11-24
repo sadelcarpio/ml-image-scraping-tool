c675bb8c0fdc7a187fe0a4b6f3a8b858006c8d85
sadelcarpio <sadelcarpioa@gmail.com>
2023-11-23 22:17:55 -0500

    Refactored Pipeline

--- beam_pipelines/pipelines/to_tfrecord/__main__.py
+++ beam_pipelines/pipelines/to_tfrecord/__main__.py
@@ -5,17 +5,37 @@ import apache_beam as beam
 
 from pipelines.to_tfrecord.image_dofns import DecodeFromTextDoFn, ReadImagesDoFn, ImageToTfExampleDoFn
 
-IMAGES_BUCKET_NAME = os.environ["IMAGES_BUCKET_NAME"]
 
-with beam.Pipeline() as p:
-    pipeline = (
-            p
-            | beam.io.ReadFromText(f'gs://{os.environ["LABELS_BUCKET"]}/data_labeled.csv', skip_header_lines=1)
-            | "Get path from gs filename" >> beam.ParDo(DecodeFromTextDoFn())
-            | "Read images from paths" >> beam.ParDo(ReadImagesDoFn(bucket_name=IMAGES_BUCKET_NAME))
-            | "Convert image bytes to TFRecord" >> beam.ParDo(ImageToTfExampleDoFn())
-            | beam.Map(lambda x: x.SerializeToString())
-            | beam.io.tfrecordio.WriteToTFRecord(file_path_prefix=f'gs://{os.environ["TFRECORD_BUCKET"]}/'
-                                                                  f'{datetime.now().strftime("%d-%m-%Y")}/images',
-                                                 shard_name_template='-SSSSS-of-NNNNN.tfrecord')
-    )
+class ToTFRecordPipeline(beam.PTransform):
+    def __init__(self, images_bucket, labels_bucket, tfrecord_bucket):
+        super().__init__()
+        self.images_bucket = images_bucket
+        self.labels_bucket = labels_bucket
+        self.tfrecord_bucket = tfrecord_bucket
+
+    def expand(self, p):
+        return (
+                p
+                | beam.io.ReadFromText(f'gs://{self.labels_bucket}/data_labeled.csv', skip_header_lines=1)
+                | "Get path from gs filename" >> beam.ParDo(DecodeFromTextDoFn())
+                | "Read images from paths" >> beam.ParDo(ReadImagesDoFn(bucket_name=self.images_bucket))
+                | "Convert image bytes to TFRecord" >> beam.ParDo(ImageToTfExampleDoFn())
+                | beam.Map(lambda x: x.SerializeToString())
+                | beam.io.tfrecordio.WriteToTFRecord(file_path_prefix=f'gs://{self.tfrecord_bucket}/'
+                                                                      f'{datetime.now().strftime("%d-%m-%Y")}/images',
+                                                     shard_name_template='-SSSSS-of-NNNNN.tfrecord')
+        )
+
+
+def run_pipeline():
+    with beam.Pipeline() as p:
+        (
+                p
+                | ToTFRecordPipeline(os.environ["IMAGES_BUCKET_NAME"],
+                                     os.environ["LABELS_BUCKET"],
+                                     os.environ["TFRECORD_BUCKET"])
+        )
+
+
+if __name__ == "__main__":
+    run_pipeline()
