21e561899e5b25dd00cf9c30ac38fbfb7c88a2c7
sadelcarpio <sadelcarpioa@gmail.com>
2024-01-04 16:01:26 -0500

    Implemented virtual nodes on consistent hashing. Consider using a BST for node search, and caching / adding a listener to the function that gets the users.

--- labelapp/url_app/src/url_dist/consistent_hashing.py
+++ labelapp/url_app/src/url_dist/consistent_hashing.py
@@ -12,22 +12,18 @@ class ConsistentHashing:
     def distribute_url(self, hashed_url, user_ids: list) -> str:
         ring_position = int(hashed_url, 16) % self.n_hash_ring
         logger.info(f"GCS URL on position {ring_position}/{self.n_hash_ring}.")
-        user_ring_positions = self._get_sorted_ring_positions(user_ids)
+        user_ring = self._get_sorted_ring_positions(user_ids)
         assigned_user_id = None
         current = 0
         while not assigned_user_id:
             if not len(user_ids):
                 break
-            elif current == len(user_ids):
-                assigned_user_id = user_ring_positions[current - 1][0]
-                logger.info(f"Assigned GCS URL {ring_position} to user with ID {user_ring_positions[current - 1][0]}"
-                            f" in position {user_ring_positions[current - 1][1]}")
-            elif ring_position >= user_ring_positions[current][1]:
-                current += 1
+            if current == len(user_ring) or ring_position < user_ring[current][1]:
+                logger.info(f"Assigned {ring_position} to user {user_ring[current - 1][0]}"
+                            f" in position {user_ring[current - 1][1]}")
+                assigned_user_id = user_ring[current - 1][0]
             else:
-                logger.info(f"Assigned {ring_position} to user {user_ring_positions[current - 1][0]}"
-                            f" in position {user_ring_positions[current - 1][1]}")
-                assigned_user_id = user_ring_positions[current - 1][0]
+                current += 1
         return assigned_user_id
 
     def _get_sorted_ring_positions(self, query: list) -> list:
--- labelapp/url_app/src/url_dist/virtual_nodes.py
+++ labelapp/url_app/src/url_dist/virtual_nodes.py
@@ -1,3 +1,14 @@
-class VirtualNodesConsistentHashing:
-    """To be implemented."""
-    pass
+from src.url_dist import ConsistentHashing
+from src.utils import sha256_hash
+
+
+class VirtualNodesConsistentHashing(ConsistentHashing):
+
+    def __init__(self, n_hash_ring, num_replicas):
+        super().__init__(n_hash_ring=n_hash_ring)
+        self.num_replicas = num_replicas
+
+    def _get_sorted_ring_positions(self, query: list) -> list:
+        virtual_nodes = [(item[0].hex, int(sha256_hash(item[0].hex + str(i)), 16) % self.n_hash_ring) for item in query for i
+                         in range(self.num_replicas)]
+        return sorted(virtual_nodes, key=lambda x: x[1])
