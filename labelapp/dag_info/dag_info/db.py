323c8c661b0f4e47593f5f065cea61f1802dd4c8
sadelcarpio <sadelcarpioa@gmail.com>
2024-01-13 10:06:32 -0500

    Reverted to instance name instead of localhost

--- labelapp/dag_info/dag_info/db.py
+++ labelapp/dag_info/dag_info/db.py
@@ -3,7 +3,7 @@ import os
 from sqlmodel import create_engine, Session
 
 engine = create_engine(f"postgresql+pg8000://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}"
-                       f"@localhost:5432/{os.environ['DB_NAME']}")
+                       f"@{os.environ['INSTANCE_NAME']}/{os.environ['DB_NAME']}")
 
 
 def get_session():
