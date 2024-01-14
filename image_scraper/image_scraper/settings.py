4fbf1ab43b83b9c66b367008bbbc5d742dcf7ca2
sadelcarpio <sadelcarpioa@gmail.com>
2024-01-14 11:00:44 -0500

    Optimized import

--- image_scraper/image_scraper/settings.py
+++ image_scraper/image_scraper/settings.py
@@ -7,7 +7,6 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 import os
-from datetime import datetime
 
 BOT_NAME = "image_scraper"
 
