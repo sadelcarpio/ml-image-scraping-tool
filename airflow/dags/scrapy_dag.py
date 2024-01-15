e2245cf3af5d15073c2b0af2218d49177bf4a116
sadelcarpio <sadelcarpioa@gmail.com>
2024-01-14 20:01:50 -0500

    FIX: project name in DAG
    PD. Need integration tests!

--- airflow/dags/scrapy_dag.py
+++ airflow/dags/scrapy_dag.py
@@ -25,7 +25,7 @@ for dag_params in dags_metadata:
             task_id="schedule-spider",
             http_conn_id="scrapyd_http_endpoint",
             endpoint='schedule.json',
-            data=f"project=image_scraper&spider=google_images_spider&scraping_project={dag_params['name']}"
+            data=f"project=image_scraper&spider=google_images_spider&scraping_project={dag_params['project']}"
                  f"&start_urls={dag_params['keywords']}",
             headers={"Content-Type": "application/x-www-form-urlencoded"},
             method='POST',
