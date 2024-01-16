7b00257cd475256bd6b0dc1ffc73abe53357c59a
sadelcarpio <sadelcarpioa@gmail.com>
2024-01-16 15:35:24 -0500

    Added specs to test_db_operations.py. Can't use SQLite for tests because of UUID functionality. Will look into scenario / smoke / integration testing

--- labelapp/url_app/tests/test_db_operations.py
+++ labelapp/url_app/tests/test_db_operations.py
@@ -4,27 +4,28 @@ from unittest.mock import MagicMock, patch
 from sqlalchemy import exc
 
 from url_app.db.db_operations import SQLSession
+from sqlalchemy.orm.session import sessionmaker, Session
 
 
 class TestSQLOperations(unittest.TestCase):
 
     def setUp(self):
-        mock_session = MagicMock()
-        self.db = MagicMock()
+        self.mock_session = MagicMock(spec=sessionmaker)
+        self.db = MagicMock(spec=Session)
+        self.mock_session.return_value = self.db
+        self.sql_session = SQLSession(session=self.mock_session)
         self.dist_strategy = MagicMock()
-        self.dist_strategy.distribute_url.return_value = 'uid1'
-        mock_session.return_value = self.db
-        self.db.query.return_value.join.return_value.filter_by.return_value.all.return_value = ['uid1', 'uid2', 'uid3']
-        self.db.query.return_value.filter_by.return_value.first.return_value.id = 1234
-        self.db_session = SQLSession(session=mock_session)
 
-    @patch('url_app.db.models.UrlModel')
+    @patch('url_app.db.db_operations.UrlModel')
     def test_upload_url(self, mock_url_model):
-        self.db_session.upload_url('https://storage.googleapis/nolose.jpg',
-                                   'abcdef',
-                                   'test-project',
-                                   self.dist_strategy)
-        self.db_session.session.assert_called_once()
+        self.db.query.return_value.join.return_value.filter_by.return_value.all.return_value = ['uid1', 'uid2', 'uid3']
+        self.dist_strategy.distribute_url.return_value = 'uid1'
+        self.db.query.return_value.filter_by.return_value.first.return_value.id = 1234
+        self.sql_session.upload_url('https://storage.googleapis/nolose.jpg',
+                                    'abcdef',
+                                    'test-project',
+                                    self.dist_strategy)
+        self.mock_session.assert_called_once()
         mock_url_model.assert_called_once_with(gcs_url='https://storage.googleapis/nolose.jpg',
                                                hashed_url='abcdef', labeled=False, project_id=1234, user_id='uid1')
         self.db.add.assert_called_once()
@@ -34,10 +35,10 @@ class TestSQLOperations(unittest.TestCase):
 
     def test_upload_url_integrity_error(self):
         self.db.commit.side_effect = exc.IntegrityError("Record already exists.", [], BaseException())
-        self.db_session.upload_url('https://storage.googleapis/nolose.jpg',
-                                   'abcdef',
-                                   'test-project',
-                                   self.dist_strategy)
+        self.sql_session.upload_url('https://storage.googleapis/nolose.jpg',
+                                    'abcdef',
+                                    'test-project',
+                                    self.dist_strategy)
         self.db.add.assert_called_once()
         self.db.commit.assert_called_once()
         self.db.refresh.assert_not_called()
@@ -47,10 +48,10 @@ class TestSQLOperations(unittest.TestCase):
     def test_upload_url_sqlalchemyerror(self):
         self.db.commit.side_effect = exc.SQLAlchemyError("Some error happened")
         with self.assertRaises(exc.SQLAlchemyError):
-            self.db_session.upload_url('https://storage.googleapis/nolose.jpg',
-                                       'abcdef',
-                                       'test-project',
-                                       self.dist_strategy)
+            self.sql_session.upload_url('https://storage.googleapis/nolose.jpg',
+                                        'abcdef',
+                                        'test-project',
+                                        self.dist_strategy)
         self.db.add.assert_called_once()
         self.db.commit.assert_called_once()
         self.db.refresh.assert_not_called()
@@ -58,21 +59,24 @@ class TestSQLOperations(unittest.TestCase):
         self.db.close.assert_called_once()
 
     def test_user_deleted(self):
+        # Normal behavior
+        self.db.query.return_value.join.return_value.filter_by.return_value.all.return_value = ['uid1', 'uid2', 'uid3']
         mock_reassign_unlabeled_urls = MagicMock()
-        self.db_session.reassign_unlabeled_urls = mock_reassign_unlabeled_urls
-        self.db_session.upload_url('https://storage.googleapis/nolose.jpg',
-                                   'abcdef',
-                                   'test-project',
-                                   self.dist_strategy)
-        self.assertEqual(3, self.db_session.n_users)
+        self.sql_session.reassign_unlabeled_urls = mock_reassign_unlabeled_urls
+        self.sql_session.upload_url('https://storage.googleapis/nolose.jpg',
+                                    'abcdef',
+                                    'test-project',
+                                    self.dist_strategy)
+        self.assertEqual(3, self.sql_session.n_users)
+        # Now as if one user were deleted
         self.db.query.return_value.join.return_value.filter_by.return_value.all.return_value = ['uid1', 'uid3']
-        self.db_session.upload_url('https://storage.googleapis/silose.jpg',
-                                   '12345',
-                                   'test-project',
-                                   self.dist_strategy)
-        self.assertEqual(2, self.db_session.n_users)
-        self.db_session.reassign_unlabeled_urls.assert_called_once_with(['uid1', 'uid3'],
-                                                                        self.dist_strategy)
+        self.sql_session.upload_url('https://storage.googleapis/silose.jpg',
+                                    '12345',
+                                    'test-project',
+                                    self.dist_strategy)
+        self.assertEqual(2, self.sql_session.n_users)
+        self.sql_session.reassign_unlabeled_urls.assert_called_once_with(['uid1', 'uid3'],
+                                                                         self.dist_strategy)
         self.assertEqual(2, len(self.dist_strategy.distribute_url.call_args_list))
 
     def test_reassign_unlabeled_urls(self):
@@ -81,7 +85,7 @@ class TestSQLOperations(unittest.TestCase):
         mock_url_1 = MagicMock(hashed_url='123')
         mock_url_2 = MagicMock(hashed_url='abc')
         self.db.query.return_value.filter_by.return_value = [mock_url_1, mock_url_2]
-        self.db_session.reassign_unlabeled_urls(user_ids, self.dist_strategy)
+        self.sql_session.reassign_unlabeled_urls(user_ids, self.dist_strategy)
         self.assertEqual(2, len(self.dist_strategy.distribute_url.call_args_list))
         self.assertEqual(2, len(self.db.commit.call_args_list))
         self.assertEqual(2, len(self.db.refresh.call_args_list))
@@ -92,7 +96,7 @@ class TestSQLOperations(unittest.TestCase):
         self.db.commit.side_effect = exc.SQLAlchemyError("Another error happened")
         self.db.query.return_value.filter_by.return_value = [MagicMock(hashed_url='123'), MagicMock(hashed_url='abc')]
         with self.assertRaises(exc.SQLAlchemyError):
-            self.db_session.reassign_unlabeled_urls(['uid1', 'uid2'], self.dist_strategy)
+            self.sql_session.reassign_unlabeled_urls(['uid1', 'uid2'], self.dist_strategy)
         self.db.commit.assert_called_once()
         self.db.refresh.assert_not_called()
         self.db.rollback.assert_called_once()
