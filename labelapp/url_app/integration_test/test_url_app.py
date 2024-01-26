45b7a1b6febde50cc1359ee0ed5e206d8e657ba6
Sergio Del Carpio <sadelcarpioa@gmail.com>
2024-01-26 14:54:37 -0500

    Tests de integración (#21)
    * Initial integration test
    
    * Added integration test to GH Actions.
    
    * Corrected
    
    * Gave permission to sh file
    
    * Add env file for testing
    
    * Better naming for workflows
    
    * Better structure for integration test

---
+++ labelapp/url_app/integration_test/test_url_app.py
@@ -0,0 +1,33 @@
+import logging
+import unittest
+from datetime import datetime
+
+from sqlalchemy import exc
+
+from integration_test.setup import URLS as EXPECTED_URLS
+from url_app.db.models import UrlModel
+from url_app.db.session import PostgreSQLSessionCreator
+
+logger = logging.getLogger(__name__)
+
+
+class UrlAppIntegrationTest(unittest.TestCase):
+
+    def setUp(self):
+        sql_session = PostgreSQLSessionCreator().create_session()
+        self.db = sql_session.session()
+
+    def test_url_in_db(self):
+        try:
+            urls = self.db.query(UrlModel).all()
+            for url, expected_url in zip(urls, EXPECTED_URLS):
+                self.assertEqual(expected_url, url.gcs_url)
+                self.assertIsNotNone(url.user_id)
+                self.assertEqual(1, url.project_id)
+                self.assertIsInstance(url.created_at, datetime)
+                self.assertIsInstance(url.updated_at, datetime)
+        except exc.SQLAlchemyError as e:
+            logger.error(f"Could not perform operations. {e}")
+            raise
+        finally:
+            self.db.close()
