d70bfea015f083882b93cb49c076867b4beab8ee
sadelcarpio <sadelcarpioa@gmail.com>
2024-02-12 14:28:34 -0500

    FIX: remove pwd dependency in docker compose

--- image_scraper/image_scraper/kafka/producer.py
+++ image_scraper/image_scraper/kafka/producer.py
@@ -7,7 +7,7 @@ from scrapy.utils.project import get_project_settings
 class KafkaProducer(confluent_kafka.Producer):
     def __init__(self, bootstrap_servers: str, client_id: str) -> None:
         self.gcs_folder_path = get_project_settings()["GCS_FOLDER_PATH"]
-        super().__init__({'bootstrap.servers': bootstrap_servers, 'client.id': client_id})
+        super().__init__({'bootstrap.servers': bootstrap_servers, 'client.id': client_id, 'acks': 'all'})
 
     def produce_urls(self, topic: str, filenames: list, scraping_project: str, prefix: str):
         for filename in filenames:
