5d933b94d38c15b41bcefc91e9b249f9e7735fe0
sadelcarpio <sadelcarpioa@gmail.com>
2024-02-20 11:35:30 -0500

    Mounting code to avoid rebuilding in tests

--- labelapp/labeling_app/app/__init__.py
+++ labelapp/labeling_app/app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 from app.api.v1.api import api_router
 from app.core.config import Settings
@@ -10,5 +11,12 @@ def create_app(settings: Settings) -> FastAPI:
         docs_url=f"{settings.API_V1_STR}/docs",
         redoc_url=f"{settings.API_V1_STR}/redoc"
     )
+
+    @app.get("/")
+    @app.get(f"{settings.API_V1_STR}/")
+    def read_root():
+        response = RedirectResponse(url=f"{settings.API_V1_STR}/docs")
+        return response
+
     app.include_router(api_router, prefix=settings.API_V1_STR)
     return app
--- labelapp/labeling_app/app/db/init_db.py
+++ labelapp/labeling_app/app/db/init_db.py
@@ -1,3 +1,4 @@
+from sqlalchemy import exc
 from sqlmodel import SQLModel
 from sqlmodel import Session
 
@@ -18,47 +19,50 @@ def init_db():
     SQLModel.metadata.create_all(engine)
     # Just for testing CRUD operations
     with Session(engine) as session:
-        users_crud = CRUDUser(UserModel, session)
-        projects_crud = CRUDProject(ProjectModel, session)
-        my_user = users_crud.create_with_pwd_hashing(obj_in=UserCreate(username="sergio",
-                                                                       email="sergio@gmail.com",
-                                                                       is_admin=True,
-                                                                       password="sacarracatelas",
-                                                                       full_name="Sergio"))
-        user_1 = users_crud.create_with_pwd_hashing(obj_in=UserCreate(username="sadel",
-                                                                      email="sadel@gmail.com",
-                                                                      is_admin=False,
-                                                                      password="sacarracatelas",
-                                                                      full_name="Sergio"))
-        user_2 = users_crud.create_with_pwd_hashing(obj_in=UserCreate(username="sadel2",
-                                                                      email="sadel2@gmail.com",
-                                                                      is_admin=False,
-                                                                      password="sacarracatelas",
-                                                                      full_name="Sergio"))
-        user = users_crud.get(my_user.id)
-        user_1 = users_crud.get(user_1.id)
-        label1 = LabelModel(name="cat")
-        label2 = LabelModel(name="dog")
-        label3 = LabelModel(name="inclination")
-        label4 = LabelModel(name="elevation")
-        projects_crud.create_with_users(obj_in=ProjectCreateWithUsers(name="test-project-full",
-                                                                      keywords="gatitos+chidos",
-                                                                      description="This is a test",
-                                                                      task_type=TaskType.sparse,
-                                                                      labels=[label1, label2],
-                                                                      user_ids=[user_1.id],
-                                                                      owner_id=user.id))
-        projects_crud.create_with_users(obj_in=ProjectCreateWithUsers(name="Airlplane Properties Prediction",
-                                                                      keywords="airplanes",
-                                                                      description="This is a test",
-                                                                      task_type=TaskType.regression,
-                                                                      labels=[label3, label4],
-                                                                      user_ids=[user_1.id, user_2.id],
-                                                                      owner_id=user.id))
-        project = projects_crud.get(1)
-        urls = [UrlModel(gcs_url=f"https://www.google.com/image_{i}",
-                         hashed_url=f"abcdef{i}",
-                         user_id=user_1.id,
-                         project_id=project.id) for i in range(10)]
-        session.add_all(urls)
-        session.commit()
+        try:
+            users_crud = CRUDUser(UserModel, session)
+            projects_crud = CRUDProject(ProjectModel, session)
+            my_user = users_crud.create_with_pwd_hashing(obj_in=UserCreate(username="sergio",
+                                                                           email="sergio@gmail.com",
+                                                                           is_admin=True,
+                                                                           password="sacarracatelas",
+                                                                           full_name="Sergio"))
+            user_1 = users_crud.create_with_pwd_hashing(obj_in=UserCreate(username="sadel",
+                                                                          email="sadel@gmail.com",
+                                                                          is_admin=False,
+                                                                          password="sacarracatelas",
+                                                                          full_name="Sergio"))
+            user_2 = users_crud.create_with_pwd_hashing(obj_in=UserCreate(username="sadel2",
+                                                                          email="sadel2@gmail.com",
+                                                                          is_admin=False,
+                                                                          password="sacarracatelas",
+                                                                          full_name="Sergio"))
+            user = users_crud.get(my_user.id)
+            user_1 = users_crud.get(user_1.id)
+            label1 = LabelModel(name="cat")
+            label2 = LabelModel(name="dog")
+            label3 = LabelModel(name="inclination")
+            label4 = LabelModel(name="elevation")
+            projects_crud.create_with_users(obj_in=ProjectCreateWithUsers(name="test-project-full",
+                                                                          keywords="gatitos+chidos",
+                                                                          description="This is a test",
+                                                                          task_type=TaskType.sparse,
+                                                                          labels=[label1, label2],
+                                                                          user_ids=[user_1.id],
+                                                                          owner_id=user.id))
+            projects_crud.create_with_users(obj_in=ProjectCreateWithUsers(name="Airlplane Properties Prediction",
+                                                                          keywords="airplanes",
+                                                                          description="This is a test",
+                                                                          task_type=TaskType.regression,
+                                                                          labels=[label3, label4],
+                                                                          user_ids=[user_1.id, user_2.id],
+                                                                          owner_id=user.id))
+            project = projects_crud.get(1)
+            urls = [UrlModel(gcs_url=f"https://www.google.com/image_{i}",
+                             hashed_url=f"abcdef{i}",
+                             user_id=user_1.id,
+                             project_id=project.id) for i in range(10)]
+            session.add_all(urls)
+            session.commit()
+        except exc.IntegrityError:
+            pass
