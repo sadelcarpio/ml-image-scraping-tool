5ce58e09dfd9681b87065e138fe641d1485d75d4
sadelcarpio <sadelcarpioa@gmail.com>
2024-04-21 22:59:51 -0500

    Basic writetocsv (should be to gcs)

--- beam_pipelines/pipelines/upload_csv_labels/__main__.py
+++ beam_pipelines/pipelines/upload_csv_labels/__main__.py
@@ -1,15 +1,24 @@
 import apache_beam as beam
 from apache_beam.io.jdbc import ReadFromJdbc
+from apache_beam.typehints.schemas import MillisInstant, LogicalType
+
+LogicalType.register_logical_type(MillisInstant)
+
+PROJECT = "test-project-full"  # pass as cmd argument
 
 with beam.Pipeline() as p:
     pipeline = (
             p
             | ReadFromJdbc(
-                query="SELECT username FROM users;",
+                query=f"SELECT label, gcs_url FROM labels_for_processing WHERE project='{PROJECT}';",
                 table_name="users",
                 driver_class_name="org.postgresql.Driver",
                 jdbc_url="jdbc:postgresql://test-postgres:5432/test",
                 username="test",
                 password="test"
-            ) | beam.Map(print)
+            )
+            | beam.Map(lambda elem: f"{elem[0]},{elem[1]}")
     )
+
+    pipeline | beam.io.WriteToText("my_labels.csv", shard_name_template="")
+    pipeline | beam.Map(print)
